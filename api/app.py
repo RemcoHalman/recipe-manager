88216079de7a7400f994561ec42ef68fd49d6630
RemcoHalman <remco.halman@gmail.com>
2021-02-28 21:10:06 +0100

    Chore: Removed db

--- api/app.py
+++
@@ -1,9 +0,0 @@
-# importing external modules
-
-import requests
-
-# import local modules
-from _db_functions import (
-    GetListCollections, GetCollectionData, 
-    DropDataCollection, DeleteDataCollection, 
-    DeleteById)
