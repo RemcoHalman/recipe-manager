7e87c73bc2702af9950bdd176b14c18ef93041fd
RemcoHalman <remco.halman@gmail.com>
2021-03-13 20:27:02 +0100

    chore: api structure updated

--- api/base/urls.py
+++ api/base/urls.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 from rest_framework import routers
 from django.urls import path, include
 
-from . import views
+from .api import views
+from apps.recipe.api import views as recipe
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'recipe', recipe.RecipeViewSet)
+router.register(r'baking', recipe.BakingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.recipe.urls')),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
